b3aaa4bd59eac82d7868b8a3459d617905e451c0
MatiasSolisBello <matias.solis.bello@gmail.com>
2025-08-11 17:53:42 -0400

    RESTART PROJECT

--- src/main/java/com/booking/spring/login/models/EDepartmentStatus.java
+++
@@ -1,7 +0,0 @@
-package com.booking.spring.login.models;
-
-public enum EDepartmentStatus {
-    AVAILABLE,
-    MAINTENANCE,
-    COMING_SOON
-}
--- src/main/java/com/booking/spring/login/repository/DepartmentImagesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.booking.spring.login.repository;
-
-import com.booking.spring.login.models.DepartmentImages;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentImagesRepository extends JpaRepository<DepartmentImages, Long> {
-}
